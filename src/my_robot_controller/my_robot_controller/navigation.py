2a7bccd7a43436d9077136ed91622f802027a43e
Ignat Jasevits <igjase@taltech.ee>
2024-04-27 12:12:21 +0300

    Task3(only src/my_robot_controller is pushed)

--- src/my_robot_controller/my_robot_controller/navigation.py
+++ src/my_robot_controller/my_robot_controller/navigation.py
@@ -2,111 +2,112 @@ import rclpy
 from rclpy.node import Node
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import PoseStamped, PoseWithCovarianceStamped
-from sensor_msgs.msg import LaserScan
-from turtlesim.msg import Pose
-import tf_transformations
 import time
-import math
+
+from tier4_system_msgs.srv import ChangeOperationMode
 
 
-class TurtleNavigationNode(Node):
+class CarNavigationNode(Node):
+  
     def __init__(self):
         super().__init__("navigation")
-        self.get_logger().info("our navigation is started")
+        self.get_logger().info("our mission planning is started")
         self.goal_poses = [] # List to store goal poses
         self.current_goal_index = 0
-
+        
         self.initial_pose_publisher = self.create_publisher(
             PoseWithCovarianceStamped, "/initialpose", 10)
-
+        
         self.goal_pose_publisher = self.create_publisher(
-            PoseStamped, "/goal_pose", 10)
+            PoseStamped, "/planning/mission_planning/goal", 10)
         
         self.odom_listener = self.create_subscription(
-            Odometry, "/odom", self.odom_callback, 10)
+            Odometry, "/localization/kinematic_state", self.odom_callback, 10)
         
-        time.sleep(5)
+        self.change_mode_srv = self.create_client(ChangeOperationMode, '/system/operation_mode/change_operation_mode')
+        self.change_mode_req = ChangeOperationMode.Request()
+        
         ############# [Initial Location] ############
         initial_pose = PoseWithCovarianceStamped()
         initial_pose.header.frame_id = 'map'
-        initial_pose.pose.pose.position.x = 0.0
-        initial_pose.pose.pose.position.y = 0.0
+        initial_pose.pose.pose.position.x = 3753.356201171875
+        initial_pose.pose.pose.position.y = 73769.984375
+        initial_pose.pose.pose.orientation.x = 0.0
+        initial_pose.pose.pose.orientation.y = 0.0
+        initial_pose.pose.pose.orientation.z = -0.5102105776693161
+        initial_pose.pose.pose.orientation.w = 0.8600495139434373
 
-        qq = tf_transformations.quaternion_from_euler(0,0,0)# x, y, z or Roll Pitch Yaw
-        initial_pose.pose.pose.orientation.x = qq[0]
-        initial_pose.pose.pose.orientation.y = qq[1]
-        initial_pose.pose.pose.orientation.z = qq[2]
-        initial_pose.pose.pose.orientation.w = qq[3]
+        time.sleep(5)
         self.initial_pose_publisher.publish(initial_pose)
         #################################
 
-        # Identify the difference between home and map reference frame
-        self.x_home = -1.8
-        self.y_home = -0.45
-
+        
         # Initialize goal poses as dictionaries {x, y, w}
-        self.goal_poses.append({'x': -3.0, 'y': 2.0, 'yaw': -30})
-        self.goal_poses.append({'x': -4.0, 'y': -5.0, 'yaw': 60})
-        self.goal_poses.append({'x': 5.0, 'y': -5.0, 'yaw': 0})
-
+        
+        self.goal_poses.append({'x': 3778.23828125, 'y': 73722.2890625, 'xx': 0.0, 'yy': 0.0, 'zz': -0.5230496544145751, 'w': 0.8523022110828962})
+        self.goal_poses.append({'x': 3808.419921875, 'y': 73718.7265625, 'xx': 0.0, 'yy': 0.0, 'zz': 0.21746131222033185, 'w': 0.9760689410525321})
+        self.goal_poses.append({'x': 3830.02099609375, 'y': 73730.1640625, 'xx': 0.0, 'yy': 0.0, 'zz': 0.20996969896452877, 'w': 0.9777078937580207})
+        
         time.sleep(5)
         self.publish_goal()
-
+      
+      
     def odom_callback(self, msg: Odometry):
         # Check if current goal pose is reached
         current_pose = msg.pose.pose
         goal_pose = self.goal_poses[self.current_goal_index]
-        distance_to_goal = (((current_pose.position.x - self.x_home) - goal_pose['x']) ** 2 + 
-                            ((current_pose.position.y - self.y_home) - goal_pose['y']) ** 2) ** 0.5
-        if distance_to_goal < 0.3: # You can adjust this threshold
+        distance_to_goal = (((current_pose.position.x) - goal_pose['x']) ** 2 +
+                            ((current_pose.position.y) - goal_pose['y']) ** 2) ** 0.5
+        if distance_to_goal < 0.3:  # You can adjust this threshold
             print(distance_to_goal)
             self.publish_next_goal()
-
+          
     def publish_next_goal(self):
         # Check if there are more goals to explore
         if self.current_goal_index < len(self.goal_poses) - 1:
             self.current_goal_index += 1
             self.publish_goal()
+          
         else:
             self.get_logger().info("All goals explored!")
             self.stop()
-
+            
+            
+    def send_request(self):
+        self.change_mode_req.mode = 2  # Modify according to your service request
+        future = self.change_mode_srv.call_async(self.change_mode_req)
 
     def publish_goal(self):
         pose_msg = PoseStamped()
         pose_msg.pose.position.x = self.goal_poses[self.current_goal_index]['x']
         pose_msg.pose.position.y = self.goal_poses[self.current_goal_index]['y']
-        yaw_angle = self.goal_poses[self.current_goal_index]['yaw']
-        qq = tf_transformations.quaternion_from_euler(0,0,yaw_angle)
 
-        pose_msg.pose.orientation.x = qq[0]
-        pose_msg.pose.orientation.y = qq[1]
-        pose_msg.pose.orientation.z = qq[2]
-        pose_msg.pose.orientation.w = qq[3]
-
+        pose_msg.pose.orientation.x = self.goal_poses[self.current_goal_index]['xx']
+        pose_msg.pose.orientation.y = self.goal_poses[self.current_goal_index]['yy']
+        pose_msg.pose.orientation.z = self.goal_poses[self.current_goal_index]['zz']
+        pose_msg.pose.orientation.w = self.goal_poses[self.current_goal_index]['w']
         pose_msg.header.frame_id = 'map'
         self.goal_pose_publisher.publish(pose_msg)
         self.get_logger().info("Published goal: {}".format(self.current_goal_index))
-
-
+        time.sleep(3)
+        self.send_request()
+        
     def stop(self):
         self.get_logger().info("stopping the node")
+        # self.destroy_node()
         rclpy.shutdown()
         raise KeyboardInterrupt
-
-
+          
 
 def main(args=None):
     rclpy.init(args=args)
-    node = TurtleNavigationNode()
+    node = CarNavigationNode()
 
     try:
         rclpy.spin(node)
     except (KeyboardInterrupt):
         node.destroy_node()
         rclpy.shutdown()
-
-
 
 if __name__ == '__main__':
     main()
--- src/my_robot_controller/setup.py
+++ src/my_robot_controller/setup.py
@@ -1,6 +1,5 @@
-import os
-from glob import glob
 from setuptools import find_packages, setup
+from glob import glob
 
 package_name = 'my_robot_controller'
 
@@ -9,22 +8,20 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', glob('launch/*.launch.py')),
-        ('share/' + package_name + '/maps', glob('maps/*'))
+        ('share/' + package_name + '/launch', glob('launch/*.launch.py'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='ignatj',
     maintainer_email='ignatj@todo.todo',
     description='TODO: Package description',
-    license='TODO: License declaration',
+    license='Apache-2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            "test_node = my_robot_controller.my_first_node:main",
-            "mapping = my_robot_controller.mapping:main",
             "navigation = my_robot_controller.navigation:main"
         ],
     },
